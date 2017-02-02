fff2870de0ff61318eb0a2d5be0c7cb562bdcd6d
zakariagatter <gatter.zaki@outlook.com>
2017-02-02 22:49:10 +0000

    New stuff :fire:

--- README.md
+++ README.md
@@ -1,32 +1,36 @@
-# MarkGate
-a very simple Oh-My-ZSH Plugin to mark Directories  
 
-## INSTALATION 
+# MarkGate
+
+a very simple Oh-My-ZSH Plugin to mark Directories
+
+## INSTALATION
 
 ```sh
 git clone https://github.com/zakariaGatter/MarkGate ~/.oh-my-zsh/custom/plugins/markgate
 ```
 
 ### Add MarkGate Plugin to ~.zshrc
+
 ---
 
-```
+```sh
 plugins=(... markgate ... )
 ```
 
-#### Usage 
+#### Usage
+
 ---
 
 * [X] : **Add Mark**
 
- ```sh
+```sh
  cd ~/.oh-my-zsh/custom/plugins/markgate
  markadd markgate
 ```
 
 or
 
-```sh
+``` sh
 markadd makgate ~/.oh-my-zsh/custom/plugins/markgate
 ```
 
@@ -39,17 +43,25 @@ markdel markgate
 ```sh
 markjumb markgate
 ```
-* [X] : **Show Marks** 
-```sh
+* [X] : **Show Marks**
+ ```sh
 markshow
-```
+ ```
 
-### Settings 
+### Settings
+
 ---
 
 To Specific **MarkGate** Config File Add this line to your ```~/.zshrc```
 
 > MARKGATE_FILE="Name_of_file"
+
+default Config File :
+
+* MARKGATE_FILE="markgate"
+
+
+``` Config File is olways on this Directorie "**~/.$MARKGATE_FILE**"```
 
 ### [!] Notes
 
@@ -57,6 +69,6 @@ If You Found any issues or have new ideos please send me a mail to :
 
 **Gmail :**
 
-```
+``` sh
 zakaria.gatter@gmail.com
 ```
--- markgate.plugin.zsh
+++ markgate.plugin.zsh
@@ -1,5 +1,15 @@
 # Zakaria [Gatter] Barkouk
 
+# # # # Color # # # #
+BRed='\e[1;31m'     #
+BGreen='\e[1;32m'   #
+BYellow='\e[1;33m'  #
+BBlue='\e[1;34m'    #
+BWhite='\e[1;37m'   #
+# # # # # # # # # # #
+Off='\e[0m'         #
+# # # # # # # # # # #
+
 #----------------------#
 # Get Config File Name #
 #----------------------#
@@ -19,31 +29,31 @@ _MARK_FILE="$HOME/.$MARK_FILE"
 # MARkGATE ADD #
 #--------------#
 function markadd () {
-if [ ! -z "$2" ];then 
+    # Check if there is to many argulent 
+    if [ "$#" -gt "2" ];then 
+        echo -e "${BYellow} ==>${BWhite} MarkGate : To Many Argument ${Off}"
+    fi 
+
     # Check if the mark are already exist
-    local _A_check=$(grep "^$1" $_MARK_FILE)
+    local _A_check=$(grep -w "$1" $_MARK_FILE)
+
+if [ ! -z "$2" ];then 
     local PATH_=$(echo "$@" | cut -d " " -f2- )
     local _PATH_=$(echo "$PATH_" | sed -e "s:^$HOME:~:" )
 
     if [ -z "$_A_check" ];then 
         echo -e "${1}=${_PATH_} " >> $_MARK_FILE
-        echo -e "MarkGate : $1 Saved "
+        echo -e "${BBlue} -->${BWhite} MarkGate : [${BGreen}${1}${BWhite}] Saved ${Off}"
     else
-        echo -e "MarkGate :\n\tThis Mark already Existed, try other Name "
+        echo -e "${BYellow} ==> MarkGate :\n\t[${BRed}${$1}${BWhite}] already Existed, try other Name ${Off}"
     fi
 else
-    # Check if the mark already exist 
-    local _A_check=$(grep "^$1" $_MARK_FILE)
     
     if [ -z "$_A_check" ];then 
-
-    # Check if the mark already exist 
-    local _A_check=$(grep "^$1" $_MARK_FILE)
-
         echo -e "${1}=${_Path} " >> $_MARK_FILE
-        echo -e "MarkGate : $1 Saved "
+        echo -e "${BBlue} -->${BWhite} MarkGate : [${BGreen}${1}${BWhite}] Saved ${Off}"
     else
-        echo -e "MarkGate :\n\tThis Mark already Existed, try other Name "
+        echo -e "${BYellow} ==>${BWhite} MarkGate :\n\t[${BRed}${1}${BWhite}] already Existed, try other Name ${Off}"
     fi
 fi
 }
@@ -52,13 +62,18 @@ fi
 # MARKGATE DELETE #
 #-----------------#
 function markdel () {
-    local _D_check=$(grep "^${1}" $_MARK_FILE)
+    # Check if there is to many argulent 
+    if [ "$#" -gt "1" ];then 
+        echo -e "${BYellow} ==>${BWhite} MarkGate : To Many Argument ${Off}"
+    fi 
+
+    local _D_check=$(grep -w "${1}" $_MARK_FILE)
 if [ -z "$_D_check" ];then 
-    echo -e "MarkGate :\n\t${1} Those not Exist "
+    echo -e "${BYellow} ==>${BWhite} MarkGate :\n\t[${BRed}${1}${BWhite}] Those not Exist ${Off}"
 else
     sed -i "/${1}/d" $_MARK_FILE
     sed -i '/^$/d' $_MARK_FILE
-    echo -e "MarkGate : ${1} Deledted"
+    echo -e "${BBlue} -->${BWhite} MarkGate : [${BGreen}${1}${BWhite}] Deledted ${Off}"
 fi
 }
 
@@ -66,12 +81,17 @@ fi
 # MARKGATE JUMB #
 #---------------#
 function markjumb () {
-    local _J_check=$(grep "^$1" $_MARK_FILE)
+    # Check if there is to many argulent 
+    if [ "$#" -gt "2" ];then 
+        echo -e "${BYellow} ==>${BWhite} MarkGate : To Many Argument ${Off}"
+    fi 
+
+    local _J_check=$(grep -w "$1" $_MARK_FILE)
 
 if [ -z "$_J_check" ];then 
-    echo -e "MarkGate :\n\t$1 : Those not Exist "
+    echo -e "${BYellow} ==>${BWhite} MarkGate :\n\t[${BRed}${1}${BWhite}] : Those not Exist ${Off}"
 else
-    local J_dir=$(cat $_MARK_FILE | grep "^$1" | cut -d "=" -f2 | tr -d " ")
+    local J_dir=$(cat $_MARK_FILE | grep -w "$1" | cut -d "=" -f2 | tr -d " ")
     local _J_dir=$(echo "$J_dir" | sed -e "s:^~:$HOME:")
     cd "${_J_dir}"
 fi
@@ -81,7 +101,13 @@ fi
 # MARKGATE SHOW #
 #---------------#
 function markshow () {
-	echo -e "\nMarkGate : \n"
-	column -t -s = $_MARK_FILE
-	echo ""
+	local Show_check=$(column -t -s = $_EDIT_FILE | wc -l )
+	if [ "$Show_check" = "0" ];then 
+		echo -e "\n${BWhite} MarkGate : "
+		echo -e "${BBlue} -->${BWhite} No Mark Set ${Of}"
+	else
+		echo -e "\n${BBlue} -->${BWhite} MarkGate : Show Mark's \n"
+		column -t -s = $_MARK_FILE
+        echo -e "${Off}"
+	fi 
 }
